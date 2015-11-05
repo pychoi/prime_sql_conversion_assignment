970e2337d5b63365f117b06c813a406d51083db4
mmpyhmm <choipui@gmail.com>
2015-11-05 17:13:27 -0600

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/sql_conversion_assignment';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({expanded: true}));
+
+// Get all the people information
+app.get('/data', function(req,res){
+    var results = [];
+
+    //SQL Query > SELECT data from table
+    pg.connect(connectionString, function (err, client, done) {
+        var query = client.query("SELECT id, name, location, age, spirit_animal, address FROM people ORDER BY name ASC");
+
+        // Stream results back one row at a time, push into results array
+        query.on('row', function (row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function () {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+// Add a new person
+app.post('/data', function(req,res){
+    console.log(req);
+
+    var addedPerson = {
+        "name" : req.body.peopleAdd,
+        "location" : req.body.locationAdd,
+        "age": req.body.ageAdd,
+        "spirit_animal": req.body.animalAdd,
+        "address": req.body.addressAdd
+    };
+
+    pg.connect(connectionString, function (err, client) {
+        //SQL Query > Insert Data
+        //Uses prepared statements, the $1 and $2 are placeholder variables. PSQL then makes sure they are relatively safe values
+        //and then uses them when it executes the query.
+
+        //var query = "INSERT INTO people (name, location) VALUES ('" + addedPerson.name + "', '" + addedPerson.location + "')";
+        //console.log(query);
+        //client.query(query);
+
+        client.query("INSERT INTO people (name, location, age, spirit_animal, address) VALUES ($1, $2, $3, $4, $5)" +
+            "RETURNING id", [addedPerson.name, addedPerson.location, addedPerson.age, addedPerson.spirit_animal, addedPerson.address],
+            function(err, result) {
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                }
+
+                res.send(true);
+            });
+
+    });
+
+});
+
+app.delete('/data', function(req,res){
+    console.log(req.body.id);
+
+    Person.findByIdAndRemove({"_id" : req.body.id}, function(err, data){
+        if(err) console.log(err);
+        res.send(data);
+    });
+
+
+});
+
+app.get("/*", function(req,res){
+    var file = req.params[0] || "/views/index.html";
+    res.sendFile(path.join(__dirname, "./public", file));
+});
+
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: ", app.get("port"));
+});
---
+++ server/public/assets/scripts/app.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+   $("#search").submit(function(event){
+      event.preventDefault();
+      var values = {};
+
+      $.each($(this).serializeArray(), function(i, field){
+         values[field.name] = field.value;
+      });
+
+      getData(values);
+   });
+
+   $("#addSomeone").submit(addSomeone);
+   $("#peopleContainer").on('click', '.delete', deletePerson);
+
+   getData();
+});
+
+function getData(values){
+   $.ajax({
+      type: "GET",
+      url: "/data",
+      data: values,
+      success: function(data){
+         updateDOM(data);
+      }
+   })
+}
+
+function addSomeone(){
+   event.preventDefault();
+   var values = {};
+
+   $.each($(this).serializeArray(), function(i, field){
+      values[field.name] = field.value;
+   });
+
+   $.ajax({
+      type: "POST",
+      url: "/data",
+      data: values,
+      success: function(data){
+         getData();
+      }
+   });
+}
+
+function deletePerson(){
+   var deletedId = {"id" : $(this).data("id")};
+
+   console.log("Meaningful Log: ", deletedId);
+
+   $.ajax({
+      type: "DELETE",
+      url: "/data",
+      data: deletedId,
+      success: function(data){
+
+         getData();
+      }
+   })
+}
+
+function updateDOM(data){
+   $("#peopleContainer").empty();
+
+   for(var i = 0; i < data.length; i++){
+      var el = "<div class='well col-md-3'>" +
+                  "<p>Name: " + data[i].name + "</p>" +
+                  "<p>Location: " + data[i].location + "</p>" +
+                  "<p>Age: " + data[i].age + "</p>" +
+                  "<p>Spirit Animal: " + data[i].spirit_animal + "</p>" +
+                  "<p>Address: " + data[i].address + "</p>" +
+                  "<button class='delete btn btn-danger' data-id='" +
+                     data[i]._id + "'>Delete</button>" +
+               "</div>";
+
+      $("#peopleContainer").append(el);
+   }
+}
